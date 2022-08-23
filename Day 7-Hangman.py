5829146f2eec8cbc1a34b8bf745d87f34d7465ee
Hephzibah Chioma Udo <41672069+hephzibah-chioma@users.noreply.github.com>
2022-08-23 18:02:45 +0100

    Add files via upload

---
+++ Day 7-Hangman.py
@@ -0,0 +1,41 @@
+
+import random
+import hangman_words
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+guessed_words = []
+
+import hangman_art
+print(hangman_art.logo)
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in guessed_words:
+        print(f"Letter {guess} has already been guessed.")
+    else: 
+        guessed_words += guess
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        if guess not in chosen_word:
+            print(f"Letter {guess} is not in the chosen word.")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You lose.")
+        print(f"{' '.join(display)}")
+        if "_" not in display:
+            end_of_game = True
+            print("You win.")
+        print(hangman_art.stages[lives])
